78b147743886f747b4be9b773328b812a22b1005
Manir Uddin <manir22sust@gmail.com>
2024-06-06 10:18:24 +0200

    Created a new product list page with a modern and user-friendly layout

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,127 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const ProductList = () => {
+  return (
+    <div className="mt-12 flex gap-8 gap-y-16 justify-between flex-wrap">
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://images.unsplash.com/photo-1542295669297-4d352b042bca?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity ease duration-500"
+          />
+          <Image
+            src="https://images.unsplash.com/photo-1508742345712-0656a285ac27?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium"> Product Name</span>
+          <span className="font-semibold"> €49</span>
+        </div>
+        <div className="text-sm text-gray-500 ">My description</div>
+        <button className="rounded-2xl w-max ring-1 ring-globalRedColor text-xs hover:bg-globalRedColor hover:text-white p-1">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://images.unsplash.com/photo-1542295669297-4d352b042bca?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity ease duration-500"
+          />
+          <Image
+            src="https://images.unsplash.com/photo-1508742345712-0656a285ac27?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium"> Product Name</span>
+          <span className="font-semibold"> €49</span>
+        </div>
+        <div className="text-sm text-gray-500 ">My description</div>
+        <button className="rounded-2xl w-max ring-1 ring-globalRedColor text-xs hover:bg-globalRedColor hover:text-white p-1">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://images.unsplash.com/photo-1542295669297-4d352b042bca?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity ease duration-500"
+          />
+          <Image
+            src="https://images.unsplash.com/photo-1508742345712-0656a285ac27?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium"> Product Name</span>
+          <span className="font-semibold"> €49</span>
+        </div>
+        <div className="text-sm text-gray-500 ">My description</div>
+        <button className="rounded-2xl w-max ring-1 ring-globalRedColor text-xs hover:bg-globalRedColor hover:text-white p-1">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://images.unsplash.com/photo-1542295669297-4d352b042bca?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity ease duration-500"
+          />
+          <Image
+            src="https://images.unsplash.com/photo-1508742345712-0656a285ac27?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium"> Product Name</span>
+          <span className="font-semibold"> €49</span>
+        </div>
+        <div className="text-sm text-gray-500 ">My description</div>
+        <button className="rounded-2xl  w-max ring-1 ring-globalRedColor text-xs hover:bg-globalRedColor hover:text-white p-1">
+          Add to Cart
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default ProductList;
