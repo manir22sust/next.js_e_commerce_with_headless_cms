af768edfbdbb4546e60719462880ba79cd624fde
Manir Uddin <manir22sust@gmail.com>
2024-06-08 23:54:47 +0200

    Created a single page layout with a clean and user-friendly design

---
+++ src/components/ProductImages.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+const images = [
+  {
+    id: 1,
+    url: "https://images.unsplash.com/photo-1513094735237-8f2714d57c13?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 2,
+    url: "https://images.unsplash.com/photo-1532453288672-3a27e9be9efd?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8",
+  },
+  {
+    id: 3,
+    url: "https://images.unsplash.com/photo-1492707892479-7bc8d5a4ee93?q=80&w=1365&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+
+  {
+    id: 4,
+    url: "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?q=80&w=1320&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+
+const ProductImages = () => {
+  const [index, setIndex] = useState(0);
+  return (
+    <div className="">
+      <div className="h-[500px] relative">
+        <Image
+          src={images[index].url}
+          alt=""
+          fill
+          sizes="50vw"
+          className="object-cover rounded-md"
+        />
+      </div>
+      <div className="flex justify-between gap-4 mt-8">
+        {images.map((img, i) => (
+          <div
+            className="w-1/4 h-32 relative gap-4 mt-8 cursor-pointer"
+            key={img.id}
+            onClick={() => setIndex(i)}
+          >
+            <Image
+              src={img.url}
+              alt=""
+              fill
+              sizes="30vw"
+              className="object-cover rounded-md"
+            />{" "}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductImages;
