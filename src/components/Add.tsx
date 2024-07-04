d51167a60af2edd2f906c4047e7e08fe8154e59f
Manir Uddin <manir22sust@gmail.com>
2024-07-04 11:03:51 +0200

    Implemented feature to add items to the shopping cart

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useWixClient } from "@/hooks/useWixClient";
 import { useState } from "react";
 
 const Add = ({
@@ -21,6 +22,23 @@ const Add = ({
     if (type === "i" && quantity < stockNumber) {
       setQuantity((prev) => prev + 1);
     }
+  };
+  const wixClient = useWixClient();
+
+  const addItem = async () => {
+    const response = await wixClient.currentCart.addToCurrentCart({
+      lineItems: [
+        {
+          catalogReference: {
+            appId: process.env.NEXT_PUBLIC_WIX_APP_ID!,
+            catalogItemId: productId,
+            ...(variantId && { options: { variantId } }),
+          },
+          quantity: quantity,
+        },
+      ],
+    });
+    console.log(response);
   };
   return (
     <div className="flex flex-col gap-4">
@@ -55,7 +73,10 @@ const Add = ({
           )}
         </div>
 
-        <button className="w-36 text-sm rounded-3xl ring-1 ring-globalRedColor text-globalRedColor py-2 px-4 hover:bg-globalRedColor hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:text-white disabled:ring-none">
+        <button
+          onClick={addItem}
+          className="w-36 text-sm rounded-3xl ring-1 ring-globalRedColor text-globalRedColor py-2 px-4 hover:bg-globalRedColor hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:text-white disabled:ring-none"
+        >
           {" "}
           Add to Cart
         </button>
